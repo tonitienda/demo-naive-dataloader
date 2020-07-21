5815e91ca6fc51704fb39703516dc354d95bbb07
Toni Tienda <tienda@libra.co.jp>
2020-07-21 17:41:44 +0900

    Printing insights

--- index.js
+++ index.js
@@ -8,41 +8,29 @@ const cache = Cache(repository.loadMany)
 const Service = require('./service')
 const { printUser } = require('./print')
 
-async function run(loader) {
+async function run(loader, label) {
+
+    console.group(label)
+    console.time('getting data')
+    repository.reset()
+
     const service = Service(loader)
 
-    console.time(`Get Users`)
-
-    console.group(`Loading user 1 twice`)
     const [user1, user1_2] = await Promise.all([service.getUserWithFriends(1, 5), service.getUserWithFriends(1, 5)])
-    console.groupEnd(`Loading user 1 twice`)
-
-    console.group(`Loading user 2`)
     const user2 = await service.getUserWithFriends(2, 5)
-    console.groupEnd(`Loading user 2`)
 
-    console.timeEnd(`Get Users`)
-
-    // printUser(user1)
-    // printUser(user1_2)
+    repository.print()
+    console.timeEnd('getting data')
+    console.groupEnd(label)
+    console.log()
 }
 
 
 async function main() {
 
-    console.group('Repository')
-    await run(repository)
-    console.groupEnd('Repository')
-
-
-    console.group('Cache')
-    await run(Cache(repository.loadMany))
-    console.groupEnd('Cache')
-
-
-    console.group('Dataloader')
-    await run(DataLoader(repository.loadMany))
-    console.groupEnd('Dataloader')
+    await run(repository, 'using repository')
+    await run(Cache(repository.loadMany), 'using cache')
+    await run(DataLoader(repository.loadMany), 'using dataloader')
 }
 
 main()
--- repository.js
+++ repository.js
@@ -1,7 +1,16 @@
 const users = require('./users.json')
 
+let requestCount = 0
+let requests = {}
+
 function loadMany(ids) {
-    console.log(`GET /users?ids=${ids}`)
+    const url = `GET /users?ids=${ids}`
+    console.log(url)
+    ids.forEach(id => {
+        requests[id] = (requests[id] || 0) + 1
+    });
+    requestCount++
+
     return new Promise(resolve => setTimeout(() => resolve(ids.map(id => users.find(u => u.id === id))), 500 + ids.length * 100))
 
 }
@@ -12,7 +21,19 @@ async function load(id) {
     return results[0]
 }
 
+function print() {
+    console.log(`Id requests: `, requests)
+    console.log(`Total requests: `, requestCount)
+}
+
+function reset() {
+    requestCount = 0
+    requests = {}
+}
+
 module.exports = {
     load,
-    loadMany
+    loadMany,
+    reset,
+    print
 }
